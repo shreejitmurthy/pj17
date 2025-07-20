a4801bae657dbb3ed8264f40ce72b6a4f314d0b8
Shreejit Murthy <botmorph17@gmail.com>
2025-07-21 01:20:07 +0930

    new map state

--- res/maps/NewStoneAutoGameJam.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="NewStoneAutoGameJam" tilewidth="8" tileheight="8" tilecount="90" columns="15">
- <image source="../images/tiles/NewStoneAutoGameJam.png" width="120" height="48"/>
-</tileset>
---
+++ res/maps/test.lua
@@ -0,0 +1,113 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 16,
+  height = 16,
+  tilewidth = 8,
+  tileheight = 8,
+  nextlayerid = 5,
+  nextobjectid = 3,
+  properties = {},
+  tilesets = {
+    {
+      name = "Stone",
+      firstgid = 1,
+      class = "",
+      tilewidth = 8,
+      tileheight = 8,
+      spacing = 0,
+      margin = 0,
+      columns = 15,
+      image = "../images/tiles/NewStoneAutoGameJam.png",
+      imagewidth = 120,
+      imageheight = 48,
+      objectalignment = "unspecified",
+      tilerendersize = "tile",
+      fillmode = "stretch",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 8,
+        height = 8
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 90,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 16,
+      height = 16,
+      id = 1,
+      name = "Stone ",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 10, 11, 12, 13, 14, 11, 12, 13, 14, 15, 0, 0, 0,
+        0, 0, 0, 25, 26, 27, 28, 29, 26, 27, 28, 29, 30, 0, 0, 0,
+        0, 0, 0, 40, 41, 43, 43, 44, 41, 42, 43, 44, 45, 0, 0, 0,
+        0, 0, 0, 55, 41, 41, 41, 41, 41, 42, 43, 44, 60, 0, 0, 0,
+        0, 0, 0, 25, 41, 42, 43, 44, 41, 42, 43, 44, 30, 0, 0, 0,
+        0, 0, 0, 40, 41, 42, 43, 44, 41, 42, 43, 44, 45, 0, 0, 0,
+        0, 0, 0, 55, 56, 57, 58, 59, 56, 57, 58, 59, 60, 0, 0, 0,
+        0, 0, 0, 70, 71, 72, 73, 74, 71, 72, 73, 74, 75, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 4,
+      name = "Ground",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 2,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 24,
+          y = 32,
+          width = 80,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
