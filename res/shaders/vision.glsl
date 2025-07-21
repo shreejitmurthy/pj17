30a1bf9c0f6a3456e3e39be2cbe2384093a37fd1
Shreejit Murthy <botmorph17@gmail.com>
2025-07-21 12:55:42 +0930

    tweak values and add uniforms

--- res/shaders/vision.glsl
+++ res/shaders/vision.glsl
@@ -5,13 +5,12 @@ extern vec2 playerDir;
 extern float coneAngle;   // in radians
 extern float coneLength;  // in pixels
 
-float coneEdgeSoftness = 0.4;
-float maxConeBrightness = 0.65;
-float ambientModifier = 0.12;
+extern float coneEdgeSoftness;
+extern float maxConeBrightness;
+extern float ambientModifier;
 
-vec4 effect(vec4 color, Image sceneTex, vec2 uv, vec2 screen_coords)
-{
-    vec4 pixel = texture(sceneTex, uv) * color;
+vec4 effect(vec4 color, Image tex, vec2 uv, vec2 screen_coords) {
+    vec4 pixel = texture(tex, uv) * color;
 
     vec2 toPixel = screen_coords - playerPos;
     float dist = length(toPixel);
@@ -27,10 +26,10 @@ vec4 effect(vec4 color, Image sceneTex, vec2 uv, vec2 screen_coords)
     float dFall = smoothstep(coneLength, coneLength * 0.75, dist);
 
     // Smoothed fade-in near eye
-    float nearFade = smoothstep(coneLength * 0.05, coneLength * 0.2, dist);
+    float nearFade = smoothstep(coneLength * 0.005, coneLength * 0.05, dist);
 
     // Combine and clamp
-    float visibility = clamp(aFall * dFall, 0.0, 1.0);
+    float visibility = clamp(aFall * dFall * nearFade, 0.0, 1.0);
 
     // Ambient blending for soft outer area
     vec3 ambient = pixel.rgb * ambientModifier;
