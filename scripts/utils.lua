1522e38fc1d9c292ac31cd8d656194bf061f98f6
Shreejit Murthy <botmorph17@gmail.com>
2025-07-21 12:56:43 +0930

    angle utils

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -6,3 +6,19 @@ function lerpc(v, g, s, dt)
     v = v + (g - v) * math.min(s * dt, 1)
     return v
 end
+
+function angleDiff(a, b)
+    local d = (a - b) % (2*math.pi)
+    if d > math.pi then d = d - 2*math.pi end
+    return d
+end
+
+-- helper to lerp angles
+function lerpAngle(a, b, t)
+    local d = (b - a + math.pi) % (2*math.pi) - math.pi
+    return a + d * t
+end
+
+function normalizeAngle(a)
+    return (a + math.pi * 2) % (math.pi * 2)
+end
